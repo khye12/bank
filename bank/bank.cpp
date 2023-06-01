89c204e46b43955adfcf4bf54ba89fc9e7e7b1be
k-hye.yun <khye12.yun@gmail.com>
2023-06-01 13:49:16 +0900

    bank initial
    Signed-off-by: k-hye.yun <khye12.yun@gmail.com>

---
+++ bank/bank.cpp
@@ -0,0 +1,20 @@
+﻿class Account
+{
+public:
+	explicit Account(int money)
+		:balance(money){}
+	int getBalance()
+	{
+		return balance;
+	}
+	void deposit(int money)
+	{
+		balance += money;
+	}
+	void Withdraw(int money)
+	{
+		balance -= money;
+	}
+private:
+	int balance;
+};
